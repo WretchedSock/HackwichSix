f2885432621e188694025263140f0cd9e9ce8912
CM Student <cmstudent@UHWO-ACM-16.local>
2018-03-05 10:31:01 -1000

    Did a lot, so pushing to HackwichSix

--- HackwichSix/ViewController.swift
+++ HackwichSix/ViewController.swift
@@ -10,16 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var slider: UISlider!
+    //create a variable that can hold the value of the slider
+    var currentValue: Int = 1
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func sliderHasMoved(_ sender: Any) {
+        print("The value of the slider is:\(slider.value)")
+        currentValue = lroundf(slider.value)
+        
+        
+    }
+    
+    @IBAction func myGuessButtonPressed(_ sender: Any) {
+        //5. New variable message thT displays a message that includes currentValue
+        let message = "The value is: \(currentValue)"
+        //1. creating the alert view
+        let alert = UIAlertController(title:"Hello World!", message: message, preferredStyle: .alert)
+        //2. creating button that user taps to dismiss view controller
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        //3. creating button to the alert view
+        alert.addAction(action)
+        //4. present alertview on the screen
+        present(alert, animated: true, completion: nil)
+        
+        
+        
+    }
 }
+
 
